a4ec09c872e4d66192caa4ea9d77faecdc069b87
Artem <xenex95@yandex.ru>
2023-01-29 22:16:29 +0500

    Создана программа для решения третьей задачи

---
+++ Exersice_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+
+Console.WriteLine("Заполните массив числами");
+
+int[] set = new int[8];
+void MyArray(int[] col)
+{
+    for (int index = 0; index < set.Length; index++)
+    {
+        Console.Write($"Введите элемент массива под индексом {index}:\t");
+        set[index] = int.Parse(Console.ReadLine());
+    }
+
+    Console.Write("Вывод массива:\t");
+
+    for (int index = 0; index < set.Length; index++)
+    {
+        Console.Write($"{set[index]} ");
+    }
+}
+
+MyArray(set);
